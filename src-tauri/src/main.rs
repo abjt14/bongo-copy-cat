0086f1f4eef8c6624903f66e584c112e9013ee37
Abhijeet Singh <abhijeetsinghmain@gmail.com>
2023-01-14 19:21:10 -0800

    added code to run rdev and inputbot conditionally based on OS

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,11 +4,16 @@
 )]
 
 use tauri::Window;
+
+#[cfg(target_os = "macos")]
 use rdev::{listen, Event};
 
+#[cfg(target_os = "windows")]
+use inputbot::KeybdKey;
+
+#[cfg(target_os = "macos")]
 #[tauri::command]
 fn listener(window: Window) {
-
     let emit_event = move || {
         if let Err(error) = window.eval("document.querySelector('button').click()") {
             println!("Error: {:?}", error)
@@ -26,6 +31,27 @@ fn listener(window: Window) {
     if let Err(error) = listen(callback) {
         println!("Error: {:?}", error)
     }
+}
+
+#[cfg(target_os = "windows")]
+#[tauri::command]
+fn listener(window: Window) {
+    let emit_event = move || {
+        if let Err(error) = window.eval("document.querySelector('button').click()") {
+            println!("Error: {:?}", error)
+        };
+    };
+
+    // Bind all keys to a common callback event.
+    KeybdKey::bind_all(move |event| {
+        match inputbot::from_keybd_key(event) {
+            Some(c) => emit_event(),
+            None => (),
+        };
+    });
+
+    // Call this to start listening for bound inputs.
+    inputbot::handle_input_events();
 }
 
 fn main() {
